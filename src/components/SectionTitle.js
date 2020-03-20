a0e43ff3cd6f40a18bf6f721776b7e1a0751e938
amr11mahmoud <amrmahmoudfcb11@gmail.com>
2020-03-20 23:31:48 +0200

    Basic Setup

---
+++ src/components/SectionTitle.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const SectionTitle = props => {
+  return (
+    <div className="section-title">
+      <h4>{props.title}</h4>
+      <div></div>
+    </div>
+  );
+};
---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class SingleRoom extends Component {
+  render() {
+    return (
+      <div>
+        <h1>hello from single page</h1>
+      </div>
+    );
+  }
+}
